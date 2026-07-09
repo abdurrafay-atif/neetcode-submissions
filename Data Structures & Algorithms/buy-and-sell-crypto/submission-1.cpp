c8c21ad1f5361727157f308ff9a2627b12160d7e
abdurrafay-atif <193976227+abdurrafay-atif@users.noreply.github.com>
2026-07-09 17:56:35 -0400

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution 
+{
+    public:
+        // brute force approach
+        // time complexity: O(n^2)
+        // space complexity: O(1)
+        int maxProfit(vector<int>& prices) 
+        {
+            // result variable, always >= 0 as profit only occurs when it is positive
+            int max_profit = 0; 
+            // nested for loop to check all possibilities
+            for(int i = 0; i < prices.size(); i++)
+            {
+                for(int j = i + 1; j < prices.size(); j++)
+                {
+                    // calculate profit at this iteration
+                    // determine if this is more profitable than current maximum
+                    int profit = prices[j] - prices[i];
+                    max_profit = max(profit, max_profit);
+                }
+            }
+            return max_profit;
+        }
+};
