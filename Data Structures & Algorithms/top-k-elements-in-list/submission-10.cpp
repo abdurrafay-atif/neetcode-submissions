a0617090969df14e1ff047b03e083fc0c5013f82
abdurrafay-atif <193976227+abdurrafay-atif@users.noreply.github.com>
2026-07-18 21:24:08 -0400

    Add: top-k-elements-in-list - submission-10

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-10.cpp
@@ -0,0 +1,31 @@
+class Solution 
+{
+    public:
+        // bucket sort solution
+        vector<int> topKFrequent(vector<int>& nums, int k) 
+        {
+            unordered_map<int, int> freqs;
+            for(auto i : nums)
+            {
+                freqs[i] = freqs.find(i) == freqs.end() ? 1 : freqs[i] + 1;
+            }
+            vector<vector<int>> buckets(nums.size() + 1);
+            for(auto i : freqs)
+            {
+                buckets[i.second].push_back(i.first);
+            }
+            vector<int> res;
+            for(int i = buckets.size() - 1; i >= 0; i--)
+            {
+                for(int j = 0; j < buckets[i].size(); j++)
+                {
+                    res.push_back(buckets[i][j]);
+                    if(res.size() >= k)
+                    {
+                        return res;
+                    }
+                }
+            }
+            return res;
+        }
+};
