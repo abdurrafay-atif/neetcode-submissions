e3c6d0351525de72b145d1c6eecc229f82cb8c6e
abdurrafay-atif <193976227+abdurrafay-atif@users.noreply.github.com>
2026-07-22 13:37:16 -0400

    Add: find-target-in-rotated-sorted-array - submission-8

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-8.cpp
@@ -0,0 +1,76 @@
+class Solution 
+{
+    public:
+        int search(vector<int>& nums, int target) 
+        {
+            int left = 0;
+            int right = nums.size() - 1;
+            while(left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if(nums[left] == target)
+                {
+                    return left;
+                }
+                if(nums[right] == target)
+                {
+                    return right;
+                }
+                if(nums[mid] == target)
+                {
+                    return mid;
+                }
+                else if(target < nums[mid])
+                {
+                    if(nums[left] < nums[mid])
+                    {
+                        if(nums[left] < target)
+                        {
+                            right = mid - 1;
+                        }
+                        else
+                        {
+                            left = mid + 1;
+                        }
+                    }
+                    else
+                    {
+                        if(nums[left] < target)
+                        {
+                            left = mid + 1;
+                        }
+                        else
+                        {
+                            right = mid - 1;
+                        }
+                    }
+                }
+                else
+                {
+                    if(nums[left] < nums[mid])
+                    {
+                        if(nums[left] < target)
+                        {
+                            left = mid + 1;
+                        }
+                        else
+                        {
+                            right = mid - 1;
+                        }
+                    }
+                    else
+                    {
+                        if(nums[left] < target)
+                        {
+                            right = mid - 1;
+                        }
+                        else
+                        {
+                            left = mid + 1;
+                        }
+                    }
+                }
+            }
+            return -1;
+        }
+};
